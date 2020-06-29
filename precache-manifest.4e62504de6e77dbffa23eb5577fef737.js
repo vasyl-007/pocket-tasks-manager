ab1910b9a21e1f7db61a60081fd0e6b8aec8a749
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-29 21:53:42 +0300

    Updates

---
+++ precache-manifest.4e62504de6e77dbffa23eb5577fef737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc19d8e57df0f981af40867e78eeb28e",
+    "url": "/pocket-tasks-manager/index.html"
+  },
+  {
+    "revision": "1921e92125d619d7c223",
+    "url": "/pocket-tasks-manager/static/css/main.7e96242c.chunk.css"
+  },
+  {
+    "revision": "59cbf51646af05232499",
+    "url": "/pocket-tasks-manager/static/js/2.205ef2b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pocket-tasks-manager/static/js/2.205ef2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1921e92125d619d7c223",
+    "url": "/pocket-tasks-manager/static/js/main.56c4ddd9.chunk.js"
+  },
+  {
+    "revision": "4b5121a8eb650bbf35f2",
+    "url": "/pocket-tasks-manager/static/js/runtime-main.f879376b.js"
+  }
+]);
